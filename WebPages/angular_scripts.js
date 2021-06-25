158b76925ab695e47217bd738e325852cce54711
Althazar-Nakamuro <61932912+Althazar-Nakamuro@users.noreply.github.com>
2021-06-25 20:46:12 +0530

    Add files via upload

---
+++ WebPages/angular_scripts.js
@@ -0,0 +1,44 @@
+angular.module('myApp', []).controller('myCtrl', function($scope) {
+                $scope.headboy_candidates = [
+                    {name:"George",image:"image 1.jpeg",class:"12A",code:"GR"},
+                    {name:"Sapnap",image:"image 2.jpeg",class:"12A",code:"SN"},
+                    {name:"Dream",image:"image 3.jpeg",class:"12A",code:"DR"},
+                    {name:"TommyInnit",image:"image 4.jpeg",class:"12A",code:"TI"},
+                    {name:"Techno",image:"image 5.jpeg",class:"12A",code:"TK"}
+                ];
+                $scope.headgirl_candidates = [
+                    {name:"George",image:"image 1.jpeg",class:"12A",code:"GR"},
+                    {name:"Sapnap",image:"image 2.jpeg",class:"12A",code:"SN"},
+                    {name:"Dream",image:"image 3.jpeg",class:"12A",code:"DR"},
+                    {name:"TommyInnit",image:"image 4.jpeg",class:"12A",code:"TI"},
+                    {name:"Techno",image:"image 5.jpeg",class:"12A",code:"TK"}
+                ];
+                $scope.cultural_candidates = [
+                    {name:"George",image:"image 1.jpeg",class:"12A",code:"GR"},
+                    {name:"Sapnap",image:"image 2.jpeg",class:"12A",code:"SN"},
+                    {name:"Dream",image:"image 3.jpeg",class:"12A",code:"DR"},
+                    {name:"TommyInnit",image:"image 4.jpeg",class:"12A",code:"TI"},
+                    {name:"Techno",image:"image 5.jpeg",class:"12A",code:"TK"}
+                ];
+                $scope.joint_candidates = [
+                    {name:"George",image:"image 1.jpeg",class:"12A",code:"GR"},
+                    {name:"Sapnap",image:"image 2.jpeg",class:"12A",code:"SN"},
+                    {name:"Dream",image:"image 3.jpeg",class:"12A",code:"DR"},
+                    {name:"TommyInnit",image:"image 4.jpeg",class:"12A",code:"TI"},
+                    {name:"Techno",image:"image 5.jpeg",class:"12A",code:"TK"}
+                ];
+                $scope.sports_candidates = [
+                    {name:"George",image:"image 1.jpeg",class:"12A",code:"GR"},
+                    {name:"Sapnap",image:"image 2.jpeg",class:"12A",code:"SN"},
+                    {name:"Dream",image:"image 3.jpeg",class:"12A",code:"DR"},
+                    {name:"TommyInnit",image:"image 4.jpeg",class:"12A",code:"TI"},
+                    {name:"Techno",image:"image 5.jpeg",class:"12A",code:"TK"}
+                ];
+                $scope.sportsvc_candidates = [
+                    {name:"George",image:"image 1.jpeg",class:"12A",code:"GR"},
+                    {name:"Sapnap",image:"image 2.jpeg",class:"12A",code:"SN"},
+                    {name:"Dream",image:"image 3.jpeg",class:"12A",code:"DR"},
+                    {name:"TommyInnit",image:"image 4.jpeg",class:"12A",code:"TI"},
+                    {name:"Techno",image:"image 5.jpeg",class:"12A",code:"TK"}
+                ];
+            });
